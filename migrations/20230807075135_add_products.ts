55828cdf0f8e5c3411011c3fbc643a54a1bb39cb
SENG lyheng <senglyheng4364@gmail.com>
2023-08-07 15:07:28 +0700

    update

---
+++ migrations/20230807075135_add_products.ts
@@ -0,0 +1,21 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  if(!(await knex.schema.hasTable("products"))){
+    return await knex.schema.createTable("products",(field)=>{
+        field.increments();
+        field.string("product_name");
+        field.string("summary");
+        field.string("description");
+        field.string("image");
+        field.integer("category_id");
+        field.timestamps();
+
+    })
+}
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
---
+++ migrations/20230807075342_add_product_categotries.ts
@@ -0,0 +1,18 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  if(!(await knex.schema.hasTable("product_categories"))){
+    return await knex.schema.createTable("product_categories",(field)=>{
+        field.increments();
+        field.string("category_name");       
+        field.timestamps();
+
+    })
+}
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
+
