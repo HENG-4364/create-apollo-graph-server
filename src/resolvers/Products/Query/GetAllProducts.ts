2f062aa109a0f5af259f361c7fb2eef9c872643b
Lyheng <senglyheng2001@gmail.com>
2023-08-16 16:44:58 +0700

    update

--- src/resolvers/Products/Query/GetAllProducts.ts
+++ src/resolvers/Products/Query/GetAllProducts.ts
@@ -1,13 +1,19 @@
-import { knx } from "src/connections/CreateKnexConnectios";
+import { knx } from 'src/connections/CreateKnexConnectios';
 
-export const GetAllProducts = async () => {
-    const GetAllProducts = await knx("products");
-    const getAllProductcategories =await knx("product_categories");
-    return GetAllProducts.map(async (item) => {
-        const Productcategory = getAllProductcategories.find((x) => x.id === item.category_id);      
-        return {
-            ...item,
-            category: Productcategory
-        }
-    })
-}
+export const GetAllProducts = async (_,{ category_id }: { category_id: number },{}) => {
+  const GetAllProductsQuery = knx('products');
+  if (category_id) {
+    GetAllProductsQuery.where({ category_id: category_id });
+  }
+  const GetAllProducts = await GetAllProductsQuery;
+  const getAllProductcategories = await knx('product_categories');
+  return GetAllProducts.map(async (item) => {
+    const Productcategory = getAllProductcategories.find(
+      (x) => x.id === item.category_id
+    );
+    return {
+      ...item,
+      category: Productcategory,
+    };
+  });
+};
